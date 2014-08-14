9d813d948232deed8b5bfa6dc099f748b41b5ea4
Anton Sharapov <asharapov2@yandex.ru>
2014-08-14 07:33:01 +0400

    -

--- test/org/echosoft/common/cli/CLDisplayTest.java
+++ test/org/echosoft/common/cli/CLDisplayTest.java
@@ -1,7 +1,38 @@
 package org.echosoft.common.cli;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.echosoft.common.cli.display.CellFormatters;
+import org.echosoft.common.cli.display.TableModel;
+import org.echosoft.common.cli.display.TableProcessor;
+import org.echosoft.common.utils.StringUtil;
+
 /**
  * @author Anton Sharapov
  */
 public class CLDisplayTest {
+
+    public static void main(final String[] args) throws Exception {
+        final List<TestBean> data = Arrays.asList(
+                new TestBean(1, "Гуси-лебеди", StringUtil.parseISODate("2014-05-15"), 1, 21398742.777f, true),
+                new TestBean(2, "ООО Ромашка", StringUtil.parseISODate("2013-30-31"), 500, 2.75f, false),
+                new TestBean(3, "ООО Фармкоп", StringUtil.parseISODate("2014-01-09"), 1004, 2.75f, false),
+                new TestBean(4, "Девять негритят", StringUtil.parseISODate("2014-04-01"), 32765, 2.75f, true),
+                new TestBean(5, "", null, 0, 6, false),
+                new TestBean(6, null, StringUtil.parseISODate("2014-05-15"), 32765, 223.654f, false),
+                new TestBean(7, "Просто какая-то строка", StringUtil.parseISODate("2014-08-02"), 31415926, 7.40f, false)
+        );
+
+        final TableModel model = new TableModel();
+        model.addColumn("id", "id", CellFormatters.INTEGER, TableModel.Alignment.RIGHT);
+        model.addColumn("name", "name", CellFormatters.OBJECT, TableModel.Alignment.LEFT);
+        model.addColumn("date", "date", CellFormatters.DATE, TableModel.Alignment.CENTER);
+        model.addColumn("count", "count", CellFormatters.INTEGER, TableModel.Alignment.RIGHT);
+        model.addColumn("price", "price", CellFormatters.FLOAT, TableModel.Alignment.RIGHT);
+        model.addColumn("total", "total", CellFormatters.FLOAT, TableModel.Alignment.RIGHT);
+
+        String rendered = TableProcessor.render(model, data);
+        System.out.println(rendered);
+    }
 }
--- test/org/echosoft/common/cli/TestBean.java
+++ test/org/echosoft/common/cli/TestBean.java
@@ -1,7 +1,29 @@
 package org.echosoft.common.cli;
 
+import java.util.Date;
+
 /**
  * @author Anton Sharapov
  */
 public class TestBean {
+
+    public int id;
+    public String name;
+    public Date date;
+    public long count;
+    public float price;
+    public boolean checked;
+
+    public TestBean(final int id, final String name, final Date date, final long count, final float price, final boolean checked) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.count = count;
+        this.price = price;
+        this.checked = checked;
+    }
+
+    public double getTotal() {
+        return count * price;
+    }
 }
