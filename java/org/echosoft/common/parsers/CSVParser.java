77903ef12fcb59c2573d17edab9d5440b3a800e5
Anton Sharapov <asharapov2@yandex.ru>
2015-02-08 19:56:40 +0300

    Улучшена поддержка спецификации RFC 4180.

--- java/org/echosoft/common/parsers/CSVParser.java
+++ java/org/echosoft/common/parsers/CSVParser.java
@@ -6,15 +6,26 @@ import java.io.Reader;
 import java.util.NoSuchElementException;
 
 /**
- * Парсер данных в формате CSV (в соответствии с RFC 4180)
+ * Парсер данных в формате CSV (в соответствии с <a href="https://tools.ietf.org/html/rfc4180">RFC 4180</a>).<br/>
+ * Пример использования:<br/>
+ * <pre>
+ *  Reader stream = ...;
+ *  CSVReader parser = new CSVParser(stream, ',');
+ *  parser.skipLines(1);    // если требуется просто пропустить заданное количество строк.
+ *  while (parser.nextLine()) {
+ *      while (parser.hasNextToken()) {
+ *          String token = parser.nextToken();
+ *      }
+ *  }
+ *  parser.close();
+ * </pre>
  *
  * @author Anton Sharapov
  */
 public class CSVParser implements Closeable, AutoCloseable {
 
     public static final char TOKENS_DELIMITER = ',';
-    public static final char TOKENS_ESCAPE_CHAR = '"';
-    public static final boolean TOKENS_ESCAPE = true;
+    public static final char TOKENS_PAYLOAD_WRAPPER = '"';
 
     private static enum State {
         BOF, UNKNOWN, NEXT_TOKEN, LAST_TOKEN_AT_LINE, LAST_TOKEN_AT_FILE, EOL, EOF
@@ -22,22 +33,26 @@ public class CSVParser implements Closeable, AutoCloseable {
 
     private final Reader reader;            // входной поток который требуется разобрать на записи и их атрибуты.
     private final char delimiter;           // символ-разделитель между токенами
-    private final boolean wrapped;          // определяет следует ли по умолчанию ожидать что все разбираемые токены должны быть обрамлены в кавычки.
+    private final char wrapper;             // символ в который может быть "завернуто" значение токена (используется если в значениях могут быть пробелы, переводы строк и прочие подобные символы).
     private int lineNum;                    // порядковый номер (начиная с 1) текущей обрабатываемой строки (для которой был возвращен последний токен)
     private int lastTokenNum;               // порядковый номер (начиная с 1) последнего возвращенного токена в строке .
     private State state;                    // определяет состояние в котором находится парсер.
-    private String nextToken;               // значение очередного токена который будет возвращен следующим вызовом метода {@link #nextToken()}. Актуален когда state = SCANNED.
+    private String nextToken;               // значение очередного токена который будет возвращен следующим вызовом метода {@link #nextToken()}. Актуален когда state in (NEXT_TOKEN, LAST_TOKEN_AT_LINE, LAST_TOKEN_AT_FILE)
     private StringBuilder buf;              // буфер для накопления содержимого текущего обрабатываемого токена.
     private int unreadChar;
 
     public CSVParser(final Reader reader) {
-        this(reader, TOKENS_DELIMITER, TOKENS_ESCAPE);
+        this(reader, TOKENS_DELIMITER, TOKENS_PAYLOAD_WRAPPER);
     }
 
-    public CSVParser(final Reader reader, final char delimiter, final boolean wrapped) {
+    public CSVParser(final Reader reader, final char delimiter) {
+        this(reader, delimiter, TOKENS_PAYLOAD_WRAPPER);
+    }
+
+    public CSVParser(final Reader reader, final char delimiter, final char wrapper) {
         this.reader = reader;
         this.delimiter = delimiter;
-        this.wrapped = wrapped;
+        this.wrapper = wrapper;
         this.lineNum = 0;
         this.lastTokenNum = 0;
         this.state = State.BOF;
@@ -45,14 +60,31 @@ public class CSVParser implements Closeable, AutoCloseable {
         this.unreadChar = -1;
     }
 
+    /**
+     * Возвращает порядковый номер текущей обрабатываемой строки (начиная с 1).
+     *
+     * @return номер текущей обрабатываемой строки.
+     */
     public int getLineNum() {
         return lineNum;
     }
 
+    /**
+     * Возвращает номер последнего прочитанного методом {@link #nextToken()} токена в текущей строке (начиная с 1).
+     *
+     * @return номер последнего прочитанного токена в строке.
+     */
     public int getLastTokenNum() {
         return lastTokenNum;
     }
 
+    /**
+     * Возвращает <code>true</code> если текущая строка содержит еще токены для чтения. Для перехода на следующую строку следует использовать вызов {@link #nextLine()}.
+     *
+     * @return <code>true</code> если текущая строка содержит еще токены для чтения.
+     * @throws IOException в случае проблем с чтением содержимого исходного потока.
+     * @throws IllegalStateException в случае если в текущей строке больше нет токенов.
+     */
     public boolean hasNextToken() throws IOException {
         ensureCurrentStateResolved();
         switch (state) {
@@ -68,6 +100,15 @@ public class CSVParser implements Closeable, AutoCloseable {
         }
     }
 
+    /**
+     * Возвращает следующий токен в текущей строке.
+     * Строка в которой выбираются токены должна быть предварительно выбрана методом {@link #nextLine}.<br/>
+     * Для проверки если ли в текущей строке еще токены для чтения следует использовать метод {@link #hasNextToken()}.<br/>
+     *
+     * @return Строка со значением следующего токена.
+     * @throws IOException в случае проблем с чтением содержимого исходного потока.
+     * @throws IllegalStateException в случае если в текущей строке больше нет токенов.
+     */
     public String nextToken() throws IOException {
         ensureCurrentStateResolved();
         switch (state) {
@@ -92,42 +133,100 @@ public class CSVParser implements Closeable, AutoCloseable {
         return result;
     }
 
+    /**
+     * Пропускает указанное количество строк.<br/>
+     *
+     * @param linesCnt количество строк которые должны быть пропущены.
+     * @return <code>true</code> если после перевода строки курсор указывает на существующую строку (то есть конец файла еще не достигнут).
+     */
+    public boolean nextLine(final int linesCnt) throws IOException {
+        for (int i = 0; state != State.EOF && i < linesCnt; i++) {
+            nextLine();
+        }
+        return state != State.EOF;
+    }
+
+    /**
+     * Переводит курсор на следующую строку.
+     * Если в текущей строке были непрочитанные токены то они игнорируются.<br/>
+     * Все пустые строки (не содержащие ни одного токена) игнорируются и пролистываются автоматически с соответствующим автоувеличением счетчика прочитанных строк.
+     *
+     * @return <code>true</code> если после перевода строки курсор указывает на существующую строку (то есть конец файла еще не достигнут).
+     */
     public boolean nextLine() throws IOException {
         if (state == State.UNKNOWN || state == State.NEXT_TOKEN) {
-            while (hasNextToken())
-                nextToken();
+            // С учетом возможности наличия токенов содержащих в себе символ перевода строки (токены обрамленные кавычками)
+            // нам все равно придется делать синтаксический разбор до конца текущей записи
+            skipLine();
         }
-        int b;
         switch (state) {
             case BOF:
             case LAST_TOKEN_AT_LINE:
             case EOL: {
-                lastTokenNum = 0;
-                lineNum++;
-                while ((b = readChar()) >= 0) {
-                    final char c = (char) b;
-                    switch (c) {
-                        case '\n':
-                            lineNum++;
-                        case '\r':
-                            break;
-                        default:
-                            unreadChar(c);
-                            state = State.UNKNOWN;
-                            return true;
-                    }
+                final int b = readChar();
+                if (b >= 0) {
+                    unreadChar((char)b);
+                    lastTokenNum = 0;
+                    lineNum++;
+                    state = State.UNKNOWN;
+                    return true;
+                } else {
+                    state = State.EOF;
+                    return false;
                 }
-                state = State.EOF;
-                return false;
             }
             case LAST_TOKEN_AT_FILE:
                 state = State.EOF;
             case EOF:
-                lastTokenNum = 0;
                 return false;
             default:
                 throw new IllegalStateException();
         }
+    }
+
+    /**
+     * Пропускает текущую обрабатываемую строку и переводит курсор на следующую строку потока.
+     *
+     * @return <code>true</code> если был достигнут символ конца строки, <code>false</code> - если был достигнут конец файла.
+     */
+    protected boolean skipLine() throws IOException {
+        int b, state = 0;
+        while ( (b=readChar()) >= 0 ) {
+            final char c = (char)b;
+            switch (state) {
+                case 0: {
+                    // обрабатываем содержимое потока не заключенное в кавычки ...
+                    if (c == '\n') {
+                        if (this.state == State.UNKNOWN || this.state == State.NEXT_TOKEN)
+                        this.state = State.EOL;
+                        return true;
+                    }
+                    if (c == wrapper)
+                        state = 1;
+                    break;
+                }
+                case 1: {
+                    // обрабатываем содержимое потока которое было заключено в кавычки в ожидании закрывающей кавычки ...
+                    if (c == wrapper)
+                        state = 2;
+                    break;
+                }
+                case 2: {
+                    // обрабатываем символ следующий за возможно "закрывающей" кавычкой токена.
+                    // если этот символ - тоже кавычка значит токен продолжается.
+                    if (c == '\n') {
+                        this.state = State.EOL;
+                        return true;
+                    }
+                    state = (c == wrapper) ? 1 : 0;
+                    break;
+                }
+                default:
+                    throw new IllegalStateException();
+            }
+        }
+        this.state = State.EOF;
+        return false;
     }
 
     protected void ensureCurrentStateResolved() throws IOException {
@@ -136,104 +235,110 @@ public class CSVParser implements Closeable, AutoCloseable {
     }
 
 
-    /**
-     * Если предполагается что отдельные токены в файле должны быть обрамлены в кавычки и другие нет
-     * то этот метод должен быть перегружен.
-     *
-     * @param tokenNum порядковый номер токена (начиная с 1) в строке.
-     * @return <code>true</code> если указанный в аргументе токен должен быть обрамлен кавычками.
-     */
-    protected boolean isTokenShouldBeWrapped(final int tokenNum) {
-        return wrapped;
-    }
+    private static final int WAIT_TOKEN_START = 0;
+    private static final int PROCESS_TOKEN_PAYLOAD_MASKED = 1;
+    private static final int PROCESS_TOKEN_PAYLOAD_UNMASKED = 2;
+    private static final int WAIT_TOKEN_END = 3;
 
     protected State scanToken() throws IOException {
-        int state = isTokenShouldBeWrapped(lastTokenNum + 1) ? 0 : 3;
+        int state = WAIT_TOKEN_START;
         int b;
+        boolean hasEmptyChars = false;
         boolean cw = false;
         while ( (b = readChar()) >= 0 ) {
             final char c = (char)b;
             switch (state) {
-                case 0: {
-                    // В ожидании начала экранированного токена ...
+                case WAIT_TOKEN_START: {
+                    // В ожидании начала произвольного токена ...
                     if (c == delimiter) {
-                        nextToken = null;
+                        nextToken = "";
                         return State.NEXT_TOKEN;
                     } else
                     if (c == '\n') {
-                        nextToken = null;
-                        return State.LAST_TOKEN_AT_LINE;
+                        if (hasEmptyChars) {
+                            nextToken = "";
+                            return State.LAST_TOKEN_AT_LINE;
+                        } else {
+                            nextToken = null;
+                            return State.EOL;
+                        }
                     } else
-                    if (c == TOKENS_ESCAPE_CHAR) {
-                        state = 1;
+                    if (c == wrapper) {
+                        state = PROCESS_TOKEN_PAYLOAD_MASKED;
                     } else
-                    if (c > ' ')
-                        throw new IllegalStateException("Illegal character '" + c + "'");
+                    if (c > ' ') {
+                        state = PROCESS_TOKEN_PAYLOAD_UNMASKED;
+                        buf.append(c);
+                    }
+                    if (c != '\r')
+                        hasEmptyChars = true;
                     break;
                 }
-                case 1: {
-                    // обработка полезного содержимого экранированного токена ...
+                case PROCESS_TOKEN_PAYLOAD_MASKED: {
+                    // обработка полезного содержимого обрамленного кавычками токена ...
                     if (cw) {
-                        if (c == TOKENS_ESCAPE_CHAR) {
+                        if (c == wrapper) {
                             cw = false;
                             buf.append(c);
-                            break;
                         } else {
-                            state = 2;
+                            state = WAIT_TOKEN_END;
+                            unreadChar(c);
                         }
                     } else {
-                        if (c == TOKENS_ESCAPE_CHAR) {
+                        if (c == wrapper) {
                             cw = true;
                         } else {
                             buf.append(c);
                         }
-                        break;
                     }
-                }
-                case 2: {
-                    // в ожидании завершения экранированного токена ...
-                    if (c == delimiter) {
-                        nextToken = buf.toString();
-                        buf.setLength(0);
-                        return State.NEXT_TOKEN;
-                    } else
-                    if (c == '\n') {
-                        nextToken = buf.toString();
-                        buf.setLength(0);
-                        return State.LAST_TOKEN_AT_LINE;
-                    } else
-                    if (c > ' ')
-                        throw new IllegalStateException("Illegal character '" + c + "'");
                     break;
                 }
-                case 3: {
+                case PROCESS_TOKEN_PAYLOAD_UNMASKED: {
                     // обработка неэкранированного токена ...
                     if (c == delimiter) {
                         nextToken = buf.toString();
                         buf.setLength(0);
                         return State.NEXT_TOKEN;
                     } else
-                    if (c == '\r') {
+                    if (c > ' ') {
+                        buf.append(c);
                         break;
-                    }
+                    } else
                     if (c == '\n') {
                         nextToken = buf.toString();
                         buf.setLength(0);
                         return State.LAST_TOKEN_AT_LINE;
                     } else {
-                        buf.append(c);
+                        state = WAIT_TOKEN_END;
                     }
+                }
+                case WAIT_TOKEN_END: {
+                    // в ожидании завершения токена (ждем разделитель токенов или символ конца строки) ...
+                    if (c == delimiter) {
+                        nextToken = buf.toString();
+                        buf.setLength(0);
+                        return State.NEXT_TOKEN;
+                    } else
+                    if (c == '\n') {
+                        nextToken = buf.toString();
+                        buf.setLength(0);
+                        return State.LAST_TOKEN_AT_LINE;
+                    } else
+                    if (c > ' ')
+                        throw new IllegalStateException("Illegal character '" + c + "'");
+                    break;
                 }
             }
         }
 
         switch (state) {
-            case 0:
-                nextToken = null;
+            case WAIT_TOKEN_START:
+                nextToken = "";
                 break;
-            case 2:
-            case 3:
+            case WAIT_TOKEN_END:
+            case PROCESS_TOKEN_PAYLOAD_UNMASKED:
                 nextToken = buf.toString();
+                buf.setLength(0);
                 break;
             default:
                 throw new IllegalStateException();
--- test/org/echosoft/common/parsers/CSVParserTest.java
+++ test/org/echosoft/common/parsers/CSVParserTest.java
@@ -10,118 +10,140 @@ import org.junit.Test;
  */
 public class CSVParserTest {
 
-    private static final String TEST1 =
-            "a,b,c,\r\n" +
-            "d,e,f,g\r\n" +
-            "\r\n" +
-            "\n" +
-            " \n" +
-            "h,,j,k, ";
-    private static final String[][] TEST1_EXPECTED = {
-            {"a", "b", "c", ""},
-            {"d", "e", "f", "g"},
-            {},
-            {},
-            {" "},
-            {"h", "",  "j", "k", " "}
-    };
-
-
-    private static final String TEST2 =
-            "\"\", \"b\"\"b\" , \"c\",\r\n" +
-            "\"d\",\"e\",\"f\",\"g\"\r\n" +
-            "\"h\r\nh\",\"\",\"j\",\"k\", \n" +
-            "\r\n" +
-            "\r\n" +
-            " \r\n" +
-            "  \"complex text\" , \"complex test \"\"2\"\"\"\n";
-    private static final String[][] TEST2_EXPECTED = {
-            {"", "b\"b", "c", null},
-            {"d", "e", "f", "g"},
-            {"h\r\nh", "",  "j", "k", null},
-            {},
-            {},
-            {null},
-            {"complex text", "complex test \"2\""}
-
-    };
-
+    private static final boolean VERBOSE = false;
 
     @Test
-    public void test1a() throws Exception {
-        final CSVParser parser = new CSVParser(new StringReader(TEST1), ',', false);
-        try {
-            while (parser.nextLine()) {
-                final int lineNum = parser.getLineNum();
-                while (parser.hasNextToken()) {
-                    final String token = parser.nextToken();
-                    final int tokenNum = parser.getLastTokenNum();
-                    final String expected = TEST1_EXPECTED[lineNum - 1][tokenNum - 1];
-                    Assert.assertEquals(expected, token);
-                    //System.out.println(lineNum + "." + tokenNum + ":\t" + token);
-                }
-            }
-        } finally {
-            parser.close();
-        }
+    public void test0() throws Exception {
+        final CSVParser parser1 = new CSVParser(new StringReader(""));
+        Assert.assertFalse(parser1.nextLine());
+
+        final CSVParser parser2 = new CSVParser(new StringReader(" "));
+        Assert.assertTrue(parser2.nextLine());
+        Assert.assertTrue(parser2.hasNextToken());
+        Assert.assertEquals("", parser2.nextToken());
+        Assert.assertFalse(parser2.hasNextToken());
+        Assert.assertFalse(parser2.nextLine());
     }
 
     @Test
-    public void test1b() throws Exception {
-        final CSVParser parser = new CSVParser(new StringReader(TEST1), ',', false);
+    public void test1() throws Exception {
+        final String src =
+                "42, 5 ,\"A good token\",test ,a bad token";
+        final CSVParser parser = new CSVParser(new StringReader(src));
         try {
-            parser.nextLine();
-            while (parser.nextLine()) {
-                final int lineNum = parser.getLineNum();
-                while (parser.hasNextToken()) {
-                    final String token = parser.nextToken();
-                    final int tokenNum = parser.getLastTokenNum();
-                    final String expected = TEST1_EXPECTED[lineNum - 1][tokenNum - 1];
-                    Assert.assertEquals(expected, token);
-                    System.out.println(lineNum + "." + tokenNum + ":\t" + token);
-                }
-            }
-        } finally {
-            parser.close();
-        }
+            parser.hasNextToken();
+            Assert.fail("Illegal parser state");
+        } catch (IllegalStateException e) { /* all is ok. */ }
+        Assert.assertTrue(parser.nextLine());
+        Assert.assertTrue(parser.hasNextToken());
+        Assert.assertEquals("42", parser.nextToken());
+        Assert.assertTrue(parser.hasNextToken());
+        Assert.assertEquals("5", parser.nextToken());
+        Assert.assertTrue(parser.hasNextToken());
+        Assert.assertEquals("A good token", parser.nextToken());
+        Assert.assertTrue(parser.hasNextToken());
+        Assert.assertEquals("test", parser.nextToken());
+        try {
+            parser.hasNextToken();
+            Assert.fail("Illegal parser state");
+        } catch (IllegalStateException e) { /* all is ok. */ }
     }
 
     @Test
-    public void test2a() throws Exception {
-        final CSVParser parser = new CSVParser(new StringReader(TEST2), ',', true);
-        try {
-            while (parser.nextLine()) {
-                final int lineNum = parser.getLineNum();
-                while (parser.hasNextToken()) {
-                    final String token = parser.nextToken();
-                    final int tokenNum = parser.getLastTokenNum();
-                    final String expected = TEST2_EXPECTED[lineNum - 1][tokenNum - 1];
-                    Assert.assertEquals(expected, token);
-                    //System.out.println(lineNum + "." + tokenNum + ":\t" + token);
-                }
-            }
-        } finally {
-            parser.close();
-        }
+    public void test2() throws Exception {
+        final String src =
+                "a,b,c,\r\n" +
+                "d,e,f,g\r\n" +
+                "\r\n" +
+                "\n" +
+                " \n" +
+                "h,,j,k, ";
+        final String[][] expectations = {
+                {"a", "b", "c", ""},
+                {"d", "e", "f", "g"},
+                {},
+                {},
+                {""},
+                {"h", "", "j", "k", ""}
+        };
+        testImpl(src, expectations, 0, VERBOSE);
     }
 
     @Test
-    public void test2b() throws Exception {
-        final CSVParser parser = new CSVParser(new StringReader(TEST2), ',', true);
-        try {
-            parser.nextLine();
+    public void test3() throws Exception {
+        final String src =
+                "\"\", \"b\"\"b\" , \"c\",\r\n" +
+                "\"d\",\"e\",\"f\",\"g\"\r\n" +
+                "\"h\r\nh\",\"\",\"j\",\"k\", \n" +
+                "\r\n" +
+                "\r\n" +
+                " \r\n" +
+                "  \"complex text\" , \"complex test \"\"2\"\"\"\n";
+        final String[][] expectations = {
+                {"", "b\"b", "c", ""},
+                {"d", "e", "f", "g"},
+                {"h\r\nh", "",  "j", "k", ""},
+                {},
+                {},
+                {""},
+                {"complex text", "complex test \"2\""}
+        };
+        testImpl(src, expectations, 0, VERBOSE);
+    }
+
+    @Test
+    public void test4() throws Exception {
+        final String src =
+                "This is a header \r\n" +
+                "This is a header 2 \r\n" +
+                ",\"\",1, 2 ,\" hello \", \"world\", 3\n" +
+                ",, ,  2  , \" test \"  , \" word\", 5 ,\n" +
+                " test , hello ,world";
+        final String[][] expectations = {
+                {},
+                {},
+                {"", "", "1", "2", " hello ", "world", "3"},
+                {"", "", "", "2", " test ", " word", "5", ""},
+                {"test", "hello", "world"}
+        };
+        testImpl(src, expectations, 2, VERBOSE);
+    }
+
+    @Test
+    public void test5() throws Exception {
+        final String src =
+                "5, 1, 3, test, \"a quick brown fox\" , 3.14 \n" +
+                "\n" +
+                "\n" +
+                ",,,\n";
+        final String[][] expectations = {
+                {"5", "1", "3", "test", "a quick brown fox", "3.14"},
+                {},
+                {},
+                {"", "", "", ""}
+        };
+        testImpl(src, expectations, 0, VERBOSE);
+    }
+
+
+    private void testImpl(final String source, final String[][] expectations, final int skipLines, final boolean verbose) throws Exception {
+        try (CSVParser parser = new CSVParser(new StringReader(source), ',')) {
+            parser.nextLine(skipLines);
             while (parser.nextLine()) {
                 final int lineNum = parser.getLineNum();
+                Assert.assertTrue("too many lines at source: " + lineNum, expectations.length >= lineNum);
+                final String[] expectedTokens = expectations[lineNum - 1];
                 while (parser.hasNextToken()) {
                     final String token = parser.nextToken();
                     final int tokenNum = parser.getLastTokenNum();
-                    final String expected = TEST2_EXPECTED[lineNum - 1][tokenNum - 1];
-                    Assert.assertEquals(expected, token);
-                    System.out.println(lineNum + "." + tokenNum + ":\t" + token);
+                    Assert.assertTrue("too many tokens at source (line " + lineNum + "): " + tokenNum, expectedTokens.length >= tokenNum);
+                    final String expected = expectedTokens[tokenNum - 1];
+                    Assert.assertEquals("bad token " + '\'' + token + "\' at pos " + lineNum + "." + tokenNum, expected, token);
+                    if (verbose)
+                        System.out.println(lineNum + "." + tokenNum + ":\t" + '\'' + token + '\'');
                 }
             }
-        } finally {
-            parser.close();
         }
     }
+
 }
